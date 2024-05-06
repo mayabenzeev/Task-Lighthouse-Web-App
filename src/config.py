f9001782305a8413b659e4640a58571199f6d547
Maya Benzeev <maya.benzeev@post.runi.ac.il>
2024-05-06 20:05:20 +0300

    trying to get google API token code

---
+++ src/config.py
@@ -0,0 +1,6 @@
+# API_KEY = 'YOUR_API_KEY_HERE'
+CLIENT_ID = '507377730080-7ff19ccntap0uf32bhkfoulf6sdinffd.apps.googleusercontent.com'
+CLIENT_SECRET = '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'
+# REFRESH_TOKEN = 'YOUR_REFRESH_TOKEN_HERE'
+API_URL = 'https://www.googleapis.com/calendar/v3/calendars/primary/events'
+TOKEN_URL = 'https://oauth2.googleapis.com/token'
